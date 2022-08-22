965c8b9ba1fd9704d847edc365e5c7e8cdec20c3
murbina <max.urbina623@gmail.com>
2022-08-22 16:20:05 -0700

    tar fixes

--- qp_ivar_trim/tests/test_qp_ivar_trim.py
+++ qp_ivar_trim/tests/test_qp_ivar_trim.py
@@ -82,7 +82,7 @@ class IvarTrimTests(PluginTestCase):
         # inserting artifacts
         in_dir = mkdtemp()
         self._clean_up_files.append(in_dir)
-        file = tarfile.open('/qp_ivar_trim/support_file/tar_file/CALM_SEP_001970_03_S265_L001.sorted.tar.gz')
+        file = tarfile.open('/qp_ivar_trim/support_file/raw_data/CALM_SEP_001970_03_S265_L001.sorted.tar.gz')
         file.extractall('/qp_ivar_trim/support_file/raw_data')
         
         fp1_1 = join(in_dir, 'CALM_SEP_001970_03_S265_L001.sorted.bam')
